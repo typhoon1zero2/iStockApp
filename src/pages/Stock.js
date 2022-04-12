4796b7878cdc365fa89c031b757279146cc5fe02
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-12 14:30:09 -0400

    Created stock file redirect from dashboard

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import stocks from "../data";
 
-function Stock() {
+export default function Stock() {
+  //useState that hold stock data
+  const [stock, setStock] = useState({});
+
+  const { symbol } = useParams();
+
+  //function to get stock data
+  const getStock = () => {
+    setStock(
+      stocks.find((element) => {
+        return element.symbol === symbol;
+      })
+    );
+  };
+
+  useEffect(() => {
+    getStock();
+  }, []);
+
+  const loaded = () => {
+    return (
+      <div className="Show">
+        {/* <h1 className="show-name">{stock.name}</h1> */}
+        {/* <h2>Symbol: {stock.symbol}</h2> */}
+        {/* <p>Last Price: ${stock.lastPrice}</p>
+        <p>Change: ${stock.change}</p>
+        <p>Day High: ${stock.high}</p>
+        <p>Day Low: ${stock.low}</p>
+        <p>Open: ${stock.open}</p> */}
+      </div>
+    );
+  };
+  const loading = <h1>Loading...</h1>;
   return (
-    <div>Stock</div>
-  )
+    <div className="stock-symbol">
+      {/* <h1>Stock : {symbol}</h1> */}
+      {stock.symbol ? loaded() : loading}
+    </div>
+  );
 }
-
-export default Stock
