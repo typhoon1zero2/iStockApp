3688cac8285fa3c28d43479b6363768f37d0f7dc
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-11 20:13:52 -0400

    Created app file && Route & renders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import {Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import About from "./pages/About";
 import DashBoard from "./pages/DashBoard";
 import Home from "./pages/Home";
 import Stock from "./pages/Stock";
+import stocks from './data';
+
 
 function App() {
+    const [ stock, setStock ] = useState({}) 
+    const data = stocks;
+    const getStock = (e)=>{
+        setStock(e)
+    }
   return (
     <>
       <main className="App">
@@ -14,8 +22,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/stocks/:symbol" element={<Stock />} />
-          <Route path="/stocks" element={<DashBoard />} />
+          <Route path="/stocks/:symbol" element={<Stock stock={stock} />} />
+          <Route path="/stocks" element={<DashBoard stocks={data} getStock={getStock} />} />
         </Routes>
       </main>
     </>
